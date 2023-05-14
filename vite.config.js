f462ade7ef01826325c519a04013508e1ce2285f
Jake <jacobtlanghel@gmail.com>
2023-05-13 23:37:33 -0400

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
   define: {
     "process.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
+  },
+  build: {
+    outDir: "dist",
   },
 });
